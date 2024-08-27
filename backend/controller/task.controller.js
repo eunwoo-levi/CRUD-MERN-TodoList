bd67560bd60cd91d3e49e9df1660ba1dee09e621
eunwoo-levi <eunwoo1341@gmail.com>
2024-08-27 22:57:22 +0900

    backend done

--- backend/controller/task.controller.js
+++ backend/controller/task.controller.js
@@ -23,4 +23,28 @@ taskController.getTask = async (req, res) => {
   }
 };
 
+taskController.updateTask = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    if (!task) {
+      throw new Error("App can not find the task");
+    }
+    const fields = Object.keys(req.body);
+    fields.map((item) => (task[item] = req.body[item]));
+    await task.save();
+    res.status(200).json({ status: "success", data: task });
+  } catch (error) {
+    res.status(400).json({ status: "fail", error });
+  }
+};
+
+taskController.deleteTask = async (req, res) => {
+  try {
+    const deleteItem = await Task.findByIdAndDelete(req.params.id);
+    res.status(200).json({ status: "success", data: deleteItem });
+  } catch (error) {
+    res.status(400).json({ status: "fail", error });
+  }
+};
+
 module.exports = taskController;
--- backend/routes/task.api.js
+++ backend/routes/task.api.js
@@ -7,12 +7,8 @@ router.post("/", taskController.createTask);
 
 router.get("/", taskController.getTask);
 
-router.put("/:id", (req, res) => {
-  res.send("update task");
-});
+router.put("/:id", taskController.updateTask);
 
-router.delete("/:id", (req, res) => {
-  res.send("delete task");
-});
+router.delete("/:id", taskController.deleteTask);
 
 module.exports = router;
